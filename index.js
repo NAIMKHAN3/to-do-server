0712169f22660eaf224db2532c951adcde717bc5
NAIMKHAN3 <naimkhan01856@gmail.com>
2022-12-28 15:36:22 +0600

    my task, complete task get method added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -10,6 +12,94 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send({ Status: true, Message: 'Todo Server is running' })
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ujhfrio.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+
+        const userCollection = client.db("todo").collection("user")
+        const todoCollection = client.db("todo").collection("task")
+        const todoCompleteCollection = client.db("todo").collection("Ctask")
+
+        app.post('/adduser', async (req, res) => {
+            const user = req.body;
+            try {
+                const filter = { email: user.email }
+                const findUser = await userCollection.findOne(filter);
+                if (findUser.email) {
+                    return res.send({ message: "user already added" });
+                }
+
+                const result = await userCollection.insertOne(user);
+                res.send(result)
+            }
+            catch {
+                res.send("cann't add user")
+            }
+        })
+        app.post('/addtask', async (req, res) => {
+            const task = req.body;
+            try {
+                const result = await todoCollection.insertOne(task);
+                res.send(result)
+            }
+            catch {
+                res.send("cann't add task")
+            }
+        })
+        app.post('/completetask', async (req, res) => {
+            const task = req.body;
+            try {
+                const result = await todoCompleteCollection.insertOne(task);
+                const id = task._id;
+                const filter = { _id: ObjectId(id) }
+                const deleteTodo = await todoCollection.deleteOne(filter);
+                console.log(deleteTodo)
+                if (deleteTodo.deletedCount) {
+                    res.send(result)
+                }
+
+
+            }
+            catch {
+                res.send("cann't add task")
+            }
+        })
+        app.get('/mytask', async (req, res) => {
+            try {
+                const email = req.query.email;
+                const filter = { email: email }
+                const result = await todoCollection.find(filter).toArray();
+                res.send(result)
+            }
+            catch {
+                res.send("cann't my task")
+            }
+        })
+        app.get('/ctask', async (req, res) => {
+            try {
+                const email = req.query.email;
+                const filter = { email: email }
+                const result = await todoCompleteCollection.find(filter).toArray();
+                res.send(result)
+            }
+            catch {
+                res.send("cann't my task")
+            }
+        })
+
+    }
+    catch {
+        res.send({ Status: false })
+    }
+}
+run().catch(e => console.log(e))
 
 app.listen(port, () => {
     console.log("Server is running", port)
